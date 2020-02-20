cf233a78b6db0753fc6a0da1f123598a94f8e9e1
Li Linfeng <lilinfeng303@outlook.com>
2020-02-20 20:23:06 +0800

    Initial Commit

---
+++ CelestiaMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CelestiaMobile
+//
+//  Created by 李林峰 on 2020/2/20.
+//  Copyright © 2020 李林峰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CelestiaMobileTests/CelestiaMobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  CelestiaMobileTests.swift
+//  CelestiaMobileTests
+//
+//  Created by 李林峰 on 2020/2/20.
+//  Copyright © 2020 李林峰. All rights reserved.
+//
+
+import XCTest
+@testable import CelestiaMobile
+
+class CelestiaMobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
