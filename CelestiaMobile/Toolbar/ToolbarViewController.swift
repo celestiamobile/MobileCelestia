85c2e95d1efb0a0aa1504b98056c3575a25a7d7a
Li Linfeng <lilinfeng303@outlook.com>
2020-02-23 21:30:39 +0800

    Add toolbar setup

--- CelestiaMobile/Toolbar/ToolbarViewController.swift
+++ CelestiaMobile/Toolbar/ToolbarViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ToolbarViewController: UIViewController {
+    private lazy var tableView = UITableView(frame: .zero, style: .plain)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setup()
     }
 
     override var preferredContentSize: CGSize {
@@ -19,4 +22,41 @@ class ToolbarViewController: UIViewController {
         set {}
     }
 
+}
+
+extension ToolbarViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // TODO: base on actions
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: cell setup
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .systemBlue
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
+
+private extension ToolbarViewController {
+    func setup() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+
+        // TODO: cell setup
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
