2ca2f032ce6584b75696dfd8e29dcca499dec631
Li Linfeng <lilinfeng303@outlook.com>
2020-02-29 20:18:29 +0800

    Add app center

--- MobileCelestia/AppDelegate.swift
+++ MobileCelestia/AppDelegate.swift
@@ -10,12 +10,23 @@ import UIKit
 
 import CelestiaCore
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        #if !DEBUG
+        MSAppCenter.start("4c46cd7d-ea97-452b-920c-4328ac062db3", withServices:[
+            MSAnalytics.self,
+            MSCrashes.self
+        ])
+        #endif
 
         window = UIWindow()
         let vc = MainViewControler()
