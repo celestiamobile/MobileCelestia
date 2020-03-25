1da60bb0287337c027f7a6eba3261927bc288dd4
Li Linfeng <lilinfeng303@outlook.com>
2020-03-25 22:11:27 +0800

    Update model

--- MobileCelestia/Info/BodyInfoModel.swift
+++ MobileCelestia/Info/BodyInfoModel.swift
@@ -33,130 +33,129 @@ extension CelestiaAppCore {
 
 extension BodyInfo {
     init(selection: CelestiaSelection) {
-        self.init(name: CelestiaAppCore.shared.simulation.universe.name(for: selection),
-                  overview: overviewForSelection(selection), selection: selection)
+        let core = CelestiaAppCore.shared
+        self.init(name: core.simulation.universe.name(for: selection),
+                  overview: core.overviewForSelection(selection), selection: selection)
     }
 }
 
-private func overviewForSelection(_ selection: CelestiaSelection) -> String {
-    if let body = selection.body {
-        return overviewForBody(body)
-    } else if let star = selection.star {
-        return overviewForStar(star)
-    } else if let dso = selection.dso {
-        return overviewForDSO(dso)
-    } else {
-        return CelestiaString("No overview available.", comment: "")
-    }
-}
-
-private func overviewForBody(_ body: CelestiaBody) -> String {
-    let core = CelestiaAppCore.shared
-    var str = ""
-
-    if body.isEllipsoid {
-        str += String(format: CelestiaString("Equatorial radius: %@", comment: ""), body.radius.radiusString)
-    } else {
-        str += String(format: CelestiaString("Size: %@", comment: ""), body.radius.radiusString)
+extension CelestiaAppCore {
+    fileprivate func overviewForSelection(_ selection: CelestiaSelection) -> String {
+        if let body = selection.body {
+            return overviewForBody(body)
+        } else if let star = selection.star {
+            return overviewForStar(star)
+        } else if let dso = selection.dso {
+            return overviewForDSO(dso)
+        } else {
+            return CelestiaString("No overview available.", comment: "")
+        }
     }
 
-    let orbit = body.orbit(at: core.simulation.time)
-    let rotation = body.rotation(at: core.simulation.time)
+    private func overviewForBody(_ body: CelestiaBody) -> String {
+        var str = ""
 
-    let orbitalPeriod: TimeInterval = orbit.isPeriodic ? orbit.period : 0
+        if body.isEllipsoid {
+            str += String(format: CelestiaString("Equatorial radius: %@", comment: ""), body.radius.radiusString)
+        } else {
+            str += String(format: CelestiaString("Size: %@", comment: ""), body.radius.radiusString)
+        }
 
-    if rotation.isPeriodic && body.type != .spacecraft {
+        let orbit = body.orbit(at: simulation.time)
+        let rotation = body.rotation(at: simulation.time)
 
-        var rotPeriod = rotation.period
+        let orbitalPeriod: TimeInterval = orbit.isPeriodic ? orbit.period : 0
 
-        var dayLength: TimeInterval = 0.0
+        if rotation.isPeriodic && body.type != .spacecraft {
 
-        if orbit.isPeriodic {
-            let siderealDaysPerYear = orbitalPeriod / rotPeriod
-            let solarDaysPerYear = siderealDaysPerYear - 1.0
-            if solarDaysPerYear > 0.0001 {
-                dayLength = orbitalPeriod / (siderealDaysPerYear - 1.0)
+            var rotPeriod = rotation.period
+
+            var dayLength: TimeInterval = 0.0
+
+            if orbit.isPeriodic {
+                let siderealDaysPerYear = orbitalPeriod / rotPeriod
+                let solarDaysPerYear = siderealDaysPerYear - 1.0
+                if solarDaysPerYear > 0.0001 {
+                    dayLength = orbitalPeriod / (siderealDaysPerYear - 1.0)
+                }
+            }
+
+            let unit: String
+
+            if rotPeriod < 2.0 {
+                rotPeriod *= 24.0
+                dayLength *= 24.0
+
+                unit = CelestiaString("hours", comment: "")
+            } else {
+                unit = CelestiaString("days", comment: "")
+            }
+            str += "\n"
+            str += String(format: CelestiaString("Sidereal rotation period: %.2f %@", comment: ""), rotPeriod, unit)
+            if dayLength != 0 {
+                str += "\n"
+                str += String(format: CelestiaString("Length of day: %.2f %@", comment: ""), dayLength, unit)
             }
         }
 
-        let unit: String
-
-        if rotPeriod < 2.0 {
-            rotPeriod *= 24.0
-            dayLength *= 24.0
-
-            unit = CelestiaString("hours", comment: "")
-        } else {
-            unit = CelestiaString("days", comment: "")
-        }
-        str += "\n"
-        str += String(format: CelestiaString("Sidereal rotation period: %.2f %@", comment: ""), rotPeriod, unit)
-        if dayLength != 0 {
+        if body.hasRings {
             str += "\n"
-            str += String(format: CelestiaString("Length of day: %.2f %@", comment: ""), dayLength, unit)
+            str += CelestiaString("Has rings", comment: "")
         }
+        if body.hasAtmosphere {
+            str += "\n"
+            str += CelestiaString("Has atmosphere", comment: "")
+        }
+
+        return str
     }
 
-    if body.hasRings {
+    private func overviewForStar(_ star: CelestiaStar) -> String {
+        var str = ""
+
+        let time = simulation.time
+
+        let celPos = star.position(at: time).offet(from: .zero)
+        let eqPos = Astro.ecliptic(toEquatorial: Astro.cel(toJ2000Ecliptic: celPos))
+        let sph = Astro.rect(toSpherical: eqPos)
+
+        let hms = DMS(decimal: sph.dx)
+        str += String(format: CelestiaString("RA: %dh %dm %.2fs", comment: ""), hms.hours, abs(hms.minutes), abs(hms.seconds))
+
         str += "\n"
-        str += CelestiaString("Has rings", comment: "")
+        let dms = DMS(decimal: sph.dy)
+        str += String(format: CelestiaString("Dec: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
+
+        return str
     }
-    if body.hasAtmosphere {
+
+    private func overviewForDSO(_ dso: CelestiaDSO) -> String {
+        var str = ""
+
+        let celPos = dso.position
+        let eqPos = Astro.ecliptic(toEquatorial: Astro.cel(toJ2000Ecliptic: celPos))
+        var sph = Astro.rect(toSpherical: eqPos)
+
+        let hms = DMS(decimal: sph.dx)
+        str += String(format: CelestiaString("RA: %dh %dm %.2fs", comment: ""), hms.hours, abs(hms.minutes), abs(hms.seconds))
+
         str += "\n"
-        str += CelestiaString("Has atmosphere", comment: "")
+        var dms = DMS(decimal: sph.dy)
+        str += String(format: CelestiaString("Dec: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
+
+        let galPos = Astro.equatorial(toGalactic: eqPos)
+        sph = Astro.rect(toSpherical: galPos)
+
+        str += "\n"
+        dms = DMS(decimal: sph.dx)
+        str += String(format: CelestiaString("L: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
+
+        str += "\n"
+        dms = DMS(decimal: sph.dy)
+        str += String(format: CelestiaString("B: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
+
+        return str
     }
-
-    return str
-}
-
-private func overviewForStar(_ star: CelestiaStar) -> String {
-    let core = CelestiaAppCore.shared
-    var str = ""
-
-    let time = core.simulation.time
-
-    let celPos = star.position(at: time).offet(from: .zero)
-    let eqPos = Astro.ecliptic(toEquatorial: Astro.cel(toJ2000Ecliptic: celPos))
-    let sph = Astro.rect(toSpherical: eqPos)
-
-    let hms = DMS(decimal: sph.dx)
-    str += String(format: CelestiaString("RA: %dh %dm %.2fs", comment: ""), hms.hours, abs(hms.minutes), abs(hms.seconds))
-
-    str += "\n"
-
-    str += "\n"
-    let dms = DMS(decimal: sph.dy)
-    str += String(format: CelestiaString("Dec: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
-
-    return str
-}
-
-private func overviewForDSO(_ dso: CelestiaDSO) -> String {
-    var str = ""
-
-    let celPos = dso.position
-    let eqPos = Astro.ecliptic(toEquatorial: Astro.cel(toJ2000Ecliptic: celPos))
-    var sph = Astro.rect(toSpherical: eqPos)
-
-    let hms = DMS(decimal: sph.dx)
-    str += String(format: CelestiaString("RA: %dh %dm %.2fs", comment: ""), hms.hours, abs(hms.minutes), abs(hms.seconds))
-
-    str += "\n"
-    var dms = DMS(decimal: sph.dy)
-    str += String(format: CelestiaString("Dec: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
-
-    let galPos = Astro.equatorial(toGalactic: eqPos)
-    sph = Astro.rect(toSpherical: galPos)
-
-    str += "\n"
-    dms = DMS(decimal: sph.dx)
-    str += String(format: CelestiaString("L: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
-
-    str += "\n"
-    dms = DMS(decimal: sph.dy)
-    str += String(format: CelestiaString("B: %d° %d′ %.2f″", comment: ""), dms.degrees, abs(dms.minutes), abs(dms.seconds))
-
-    return str
 }
 
 private extension Float {
