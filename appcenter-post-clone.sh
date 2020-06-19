2bee78d301dfa04fa245c4dbde686b27b7488004
Levin Li <lilinfeng303@outlook.com>
2020-06-19 14:27:32 +0800

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,17 +2,19 @@
 
 cd $APPCENTER_SOURCE_DIRECTORY/..
 
+echo "Building for branch $APPCENTER_BRANCH"
+
 # Clone the Celestia repo (modified)
-git clone https://github.com/eyvallah/Celestia
+git clone https://github.com/eyvallah/Celestia --branch $APPCENTER_BRANCH --single-branch
 cd Celestia
 git submodule update --init
 cd ..
 
 # Clone the CelestiaCore repo
-git clone https://${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}@github.com/eyvallah/CelestiaCore
+git clone https://${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}@github.com/eyvallah/CelestiaCore --branch $APPCENTER_BRANCH --single-branch
 cd CelestiaCore
 git submodule update --init
-ln -sf libs/iOS thirdparty
+ln -sf libs/dependency/ios thirdparty
 cd ..
 
 # Install gettext, needed for translation
@@ -21,7 +23,6 @@ brew install gettext
 # Download AppCenter
 cd $APPCENTER_SOURCE_DIRECTORY
 brew install wget
-APPCENTER_VERSION="3.2.0"
 wget https://github.com/microsoft/appcenter-sdk-apple/releases/download/${APPCENTER_VERSION}/AppCenter-SDK-Apple-${APPCENTER_VERSION}.zip
 unzip -qq AppCenter-SDK-Apple-${APPCENTER_VERSION}.zip 'AppCenter-SDK-Apple/iOS/*'
 ln -sf AppCenter-SDK-Apple/iOS AppCenter
