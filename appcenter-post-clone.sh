5594446cbdc5b380af0adcbe874b1afbbec42d7b
Levin Li <lilinfeng303@outlook.com>
2020-06-28 10:20:30 +0800

    Update appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,13 +5,13 @@ cd $APPCENTER_SOURCE_DIRECTORY/..
 echo "Building for branch $APPCENTER_BRANCH"
 
 # Clone the Celestia repo (modified)
-git clone https://github.com/eyvallah/Celestia --branch $APPCENTER_BRANCH --single-branch
+git clone https://github.com/${GITHUB_USERNAME}/Celestia --branch $APPCENTER_BRANCH --single-branch
 cd Celestia
 git submodule update --init
 cd ..
 
 # Clone the CelestiaCore repo
-git clone https://${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}@github.com/eyvallah/CelestiaCore --branch $APPCENTER_BRANCH --single-branch
+git clone https://${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_USERNAME}/CelestiaCore --branch $APPCENTER_BRANCH --single-branch
 cd CelestiaCore
 git submodule update --init
 ln -sf libs/dependency/ios thirdparty
