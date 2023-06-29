b70b0f43de159de521cc1fdc91bec82bc0730134
Levin Li <lilinfeng303@outlook.com>
2023-06-29 09:16:55 +0800

    visionOS support with rendering to metal texture
    Uncheck Based on dependency analysis
    
    Remove useless definitions

---
+++ CelestiaXRCore/CXCFontCollection.h
@@ -0,0 +1,31 @@
+//
+// CXCFontCollection.h
+//
+// Copyright © 2023 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class CXCFont;
+
+NS_SWIFT_NAME(FontCollection)
+@interface CXCFontCollection : NSObject
+
+@property (readonly, nonatomic) CXCFont *mainFont;
+@property (readonly, nonatomic) CXCFont *titleFont;
+@property (readonly, nonatomic) CXCFont *normalRenderFont;
+@property (readonly, nonatomic) CXCFont *largeRenderFont;
+
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithMainFont:(CXCFont *)mainFont titleFont:(CXCFont *)titleFont normalRenderFont:(CXCFont *)normalRenderFont largeRenderFont:(CXCFont *)largeRenderFont;
+
+@end
+
+NS_ASSUME_NONNULL_END
