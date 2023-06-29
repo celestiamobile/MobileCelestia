4c278e77e0ac83944c432f6ff083f976e3dc33fd
Levin Li <lilinfeng303@outlook.com>
2023-06-29 09:16:55 +0800

    visionOS support with rendering to metal texture

---
+++ CelestiaXR/Addons/AddonCategoriesView.swift
@@ -0,0 +1,38 @@
+//
+// AddonCategoryView.swift
+//
+// Copyright © 2023 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+import CelestiaCore
+import CelestiaUI
+import SwiftUI
+
+struct AddonCategoriesView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UINavigationController
+
+    @Environment(XRRenderer.self) private var renderer
+    let resourceManager: ResourceManager
+    let requestHandler: RequestHandler
+
+    func makeUIViewController(context: Context) -> UINavigationController {
+        let baseURL = "https://celestia.mobi/resources/categories"
+        var components = URLComponents(string: baseURL)!
+        components.queryItems = [
+            URLQueryItem(name: "lang", value: AppCore.language),
+            URLQueryItem(name: "theme", value: "dark"),
+            URLQueryItem(name: "platform", value: "ios"),
+            URLQueryItem(name: "api", value: "1"),
+        ]
+        let url = components.url!
+        return UINavigationController(rootViewController: CommonWebViewController(executor: renderer, resourceManager: resourceManager, url: url, requestHandler: requestHandler, actionHandler: { _, _ in }, filterURL: false))
+    }
+
+    func updateUIViewController(_ uiViewController: UINavigationController, context: Context) {
+    }
+}
---
+++ CelestiaXR/GoTo/GoToView.swift
@@ -0,0 +1,32 @@
+//
+// GoToView.swift
+//
+// Copyright © 2023 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+import CelestiaUI
+import SwiftUI
+
+struct GoToView: UIViewControllerRepresentable {
+    typealias UIViewControllerType = GoToContainerViewController
+
+    @Environment(XRRenderer.self) private var renderer
+
+    func makeUIViewController(context: Context) -> GoToContainerViewController {
+        return GoToContainerViewController(executor: renderer) { location in
+            renderer.enqueue { core in
+                core.simulation.go(to: location)
+            }
+        } textInputHandler: { viewController, title, text, keyboardType in
+            return await viewController.getTextInput(title, text: text, keyboardType: keyboardType)
+        }
+    }
+
+    func updateUIViewController(_ uiViewController: GoToContainerViewController, context: Context) {
+    }
+}
