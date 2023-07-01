a00e197f83cfbc99327d7550633d7ac9172e7779
Levin Li <lilinfeng303@outlook.com>
2023-07-01 20:45:32 +0800

    Add CelestiaUI target

---
+++ CelestiaUI/CelestiaUI.h
@@ -0,0 +1,22 @@
+//
+// CelestiaUI.h
+//
+// Copyright © 2023 Celestia Development Team. All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CelestiaUI.
+FOUNDATION_EXPORT double CelestiaUIVersionNumber;
+
+//! Project version string for CelestiaUI.
+FOUNDATION_EXPORT const unsigned char CelestiaUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CelestiaUI/PublicHeader.h>
+
+
