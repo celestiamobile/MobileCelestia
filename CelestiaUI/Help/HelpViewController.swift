81e914574d044d151f256d208b32ae5c439c5dce
Levin Li <lilinfeng303@outlook.com>
2025-06-25 11:31:06 -0700

    Keep reference to navVc in Help

--- CelestiaUI/Help/HelpViewController.swift
+++ CelestiaUI/Help/HelpViewController.swift
@@ -19,22 +19,7 @@ public final class HelpViewController: UIViewController {
     private let requestHandler: RequestHandler
     private let assetProvider: AssetProvider
 
-    public init(executor: AsyncProviderExecutor, resourceManager: ResourceManager, requestHandler: RequestHandler, assetProvider: AssetProvider, actionHandler: ((CommonWebViewController.WebAction, UIViewController) -> Void)?) {
-        self.assetProvider = assetProvider
-        self.executor = executor
-        self.resourceManager = resourceManager
-        self.actionHandler = actionHandler
-        self.requestHandler = requestHandler
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    public override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private lazy var nav: UINavigationController = {
         let url = URL.fromGuideShort(path: "/help/welcome", language: AppCore.language, shareable: false)
         let assetProvider = self.assetProvider
         let vc = FallbackWebViewController(executor: executor, resourceManager: resourceManager, url: url, requestHandler: requestHandler, actionHandler: actionHandler, fallbackViewControllerCreator: OnboardViewController(assetProvider: assetProvider) { [weak self] action in
@@ -53,6 +38,25 @@ public final class HelpViewController: UIViewController {
         })
         let nav = BaseNavigationController(rootViewController: vc)
         nav.setNavigationBarHidden(true, animated: false)
+        return nav
+    }()
+
+    public init(executor: AsyncProviderExecutor, resourceManager: ResourceManager, requestHandler: RequestHandler, assetProvider: AssetProvider, actionHandler: ((CommonWebViewController.WebAction, UIViewController) -> Void)?) {
+        self.assetProvider = assetProvider
+        self.executor = executor
+        self.resourceManager = resourceManager
+        self.actionHandler = actionHandler
+        self.requestHandler = requestHandler
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
         install(nav)
         observeWindowTitle(for: nav)
     }
