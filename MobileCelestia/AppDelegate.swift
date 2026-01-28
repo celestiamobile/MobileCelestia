961d68dae36c2626f8a8a887a053b21a56c88603
Levin Li <lilinfeng303@outlook.com>
2026-01-28 14:37:12 -0800

    Add CelestiaActor

--- MobileCelestia/AppDelegate.swift
+++ MobileCelestia/AppDelegate.swift
@@ -136,6 +136,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        CelestiaActor.underlyingExecutor = executor
 
         AppCore.setUpLocale()
 
