bd87673e943614a8962649aed96f27f7a0808a4c
Levin Li <lilinfeng303@outlook.com>
2026-01-28 14:37:12 -0800

    Add CelestiaActor

--- CelestiaUI/Core/AsyncProviderExecutor.swift
+++ CelestiaUI/Core/AsyncProviderExecutor.swift
@@ -11,6 +11,54 @@ import CelestiaCore
 import Foundation
 
 public protocol AsyncProviderExecutor: Sendable {
-    func run(_ task: @escaping @Sendable (AppCore) -> Void) async
-    func get<T>(_ task: @escaping @Sendable (AppCore) -> T) async -> T
+    func runAsynchronously(_ task: @escaping @Sendable (AppCore) -> Void)
+}
+
+public extension AsyncProviderExecutor {
+    func run(_ task: @escaping @Sendable (AppCore) -> Void) async {
+        await withCheckedContinuation { (continuation: CheckedContinuation<Void, Never>) in
+            self.runAsynchronously {
+                task($0)
+                continuation.resume(returning: ())
+            }
+        }
+    }
+
+    func get<T>(_ task: @escaping @Sendable (AppCore) -> T) async -> T {
+        return await withCheckedContinuation { continuation in
+            self.runAsynchronously {
+                continuation.resume(returning: task($0))
+            }
+        }
+    }
+}
+
+@globalActor public actor CelestiaActor: GlobalActor {
+    static public let shared = CelestiaActor()
+    static public var underlyingExecutor: AsyncProviderExecutor? {
+        get { CelestiaSerialExecutor.shared.underlyingExecutor }
+        set { CelestiaSerialExecutor.shared.underlyingExecutor = newValue }
+    }
+
+    nonisolated public var unownedExecutor: UnownedSerialExecutor {
+        CelestiaSerialExecutor.shared.asUnownedSerialExecutor()
+    }
+}
+
+final class CelestiaSerialExecutor: SerialExecutor, @unchecked Sendable {
+    static let shared: CelestiaSerialExecutor = CelestiaSerialExecutor()
+    nonisolated(unsafe) var underlyingExecutor: AsyncProviderExecutor?
+
+    func enqueue(_ job: UnownedJob) {
+        guard let underlyingExecutor else {
+            fatalError("underlyingExecutor is not set on CelestiaSerialExecutor")
+        }
+        underlyingExecutor.runAsynchronously { _ in
+            job.runSynchronously(on: self.asUnownedSerialExecutor())
+        }
+    }
+
+    func asUnownedSerialExecutor() -> UnownedSerialExecutor {
+        return UnownedSerialExecutor(ordinary: self)
+    }
 }
