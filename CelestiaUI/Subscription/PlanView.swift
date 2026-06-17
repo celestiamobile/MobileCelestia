34a61f2b7d65001e2e9cfc4459496b8d143fb562
Levin Li <lilinfeng303@outlook.com>
2026-06-17 08:19:41 -0700

    Revert "Make subscription price labels equal size to comply with App Store review"
    This reverts commit 9b46bb90305031ec75335e5d24a97e7a386c3749.

--- CelestiaUI/Subscription/PlanView.swift
+++ CelestiaUI/Subscription/PlanView.swift
@@ -51,8 +51,8 @@ final class PlanView: UIView {
         labels.append(nameLabel)
 
         if !isCurrent {
-            let priceLabel = UILabel(textStyle: .footnote)
-            priceLabel.textColor = .label
+            let priceLabel = UILabel(textStyle: .body)
+            priceLabel.textColor = .secondaryLabel
             priceLabel.numberOfLines = 0
             priceLabel.attributedText = NSAttributedString(primaryPrice)
             labels.append(priceLabel)
